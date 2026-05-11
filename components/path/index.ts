f6b5dfc22497df96df0eb06ab8ad014d7546ce82
Bappy <bups.co@gmail.com>
2026-05-11 08:35:37 +0600

    feat(path): barrel export for components/path

--- components/path/index.ts
+++ components/path/index.ts
@@ -1,2 +1,6 @@
 export { PresetPath } from './PresetPath';
 export type { PresetPathProps } from './PresetPath';
+export { FlatPath } from './FlatPath';
+export type { FlatPathProps } from './FlatPath';
+export { PathSwitcher } from './PathSwitcher';
+export type { PathView, PathSwitcherProps } from './PathSwitcher';
